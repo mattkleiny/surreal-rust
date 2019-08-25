ecba8dbadc3d69b894c538bd4a992cdaf597e137
Matt Kleinschafer <xeusalmighty@gmail.com>
2019-08-25 14:41:23 +1000

    Implement basic linear interpolation and fix a few tests.

--- src/maths/easing.rs
+++ src/maths/easing.rs
@@ -7,19 +7,61 @@ pub trait Lerp<T> {
   fn lerp(from: T, to: T, amount: f64) -> Self;
 }
 
-impl<T: Copy + Sized + Into<f64> + From<f64>> Lerp<T> for T {
+impl Lerp<u8> for u8 {
   #[inline]
-  fn lerp(a: T, b: T, t: f64) -> Self {
-    let a: f64 = a.into();
-    let b: f64 = b.into();
+  fn lerp(a: Self, b: Self, t: f64) -> Self {
+    let a = a as f64;
+    let b = b as f64;
 
-    (a + t * (b - a)).into()
+    (a + t * (b - a)) as Self
+  }
+}
+
+impl Lerp<u32> for u32 {
+  #[inline]
+  fn lerp(a: Self, b: Self, t: f64) -> Self {
+    let a = a as f64;
+    let b = b as f64;
+
+    (a + t * (b - a)) as Self
+  }
+}
+
+impl Lerp<u64> for u64 {
+  #[inline]
+  fn lerp(a: Self, b: Self, t: f64) -> Self {
+    let a = a as f64;
+    let b = b as f64;
+
+    (a + t * (b - a)) as Self
+  }
+}
+
+impl Lerp<f32> for f32 {
+  #[inline]
+  fn lerp(a: Self, b: Self, t: f64) -> Self {
+    let a = a as f64;
+    let b = b as f64;
+
+    (a + t * (b - a)) as Self
+  }
+}
+
+impl Lerp<f64> for f64 {
+  #[inline]
+  fn lerp(a: Self, b: Self, t: f64) -> Self {
+    a + t * (b - a)
   }
 }
 
 impl Lerp<Color> for Color {
   #[inline]
-  fn lerp(_a: Color, _b: Color, _t: f64) -> Self {
-    unimplemented!()
+  fn lerp(a: Color, b: Color, t: f64) -> Self {
+    Color::RGBA(
+      u8::lerp(a.r, b.r, t),
+      u8::lerp(a.g, b.g, t),
+      u8::lerp(a.b, b.b, t),
+      u8::lerp(a.a, b.a, t),
+    )
   }
 }
