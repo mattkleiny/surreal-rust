659b1c78a6a0173f260e6b7dc17ca4b2a714e984
Matt Kleinschafer <matt.kleinschafer@xero.com>
2019-08-30 11:57:21 +1000

    Stub some more ideas related to scripting.

--- src/scripting.rs
+++ src/scripting.rs
@@ -7,6 +7,9 @@ use crate::diagnostics::*;
 
 // TODO: implement an in-game console based on the script engine (lua).
 // TODO: support 'interactive debugging' using an in-game console.
+// TODO: implement implicit entity/component binding access (entity1.health or entity1.sprite.pivot = 50) to allow easy mutation from scripts
+// TODO: replace specs with something more generally mutable, as this would simplify interaction from scripts and more rapid prototyping
+// TODO: implement broadcast groups (ala godot) to allow simple event-like system.
 // TODO: build a console utility using imgui that will allow execution of arbitrary commands/display log output.
 
 /// A scripting engine for Lua.
