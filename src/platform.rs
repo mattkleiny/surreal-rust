8aa867f45020040e8f1f503fb6a009e532ef2477
Matt Kleinschafer <xeusalmighty@gmail.com>
2020-06-23 21:58:58 +1000

    Get a window displaying.

--- src/platform.rs
+++ src/platform.rs
@@ -1,18 +1,50 @@
 //! Platform abstractions and utilities.
 
-/// An abstraction over the selected backend for the system.
-pub trait Platform {
-  type Host;
-  type Error;
+use std::process::exit;
 
-  /// Builds the host for the platform.
-  fn build(&self) -> Result<Self::Host, Self::Error>;
+use luminance::context::GraphicsContext;
+use luminance::pipeline::PipelineState;
+use luminance_glfw::{Action, GlfwSurface, Key, Surface, WindowDim, WindowEvent, WindowOpt};
+
+/// Configuration for the platform.
+#[derive(Clone, Debug)]
+pub struct Config {
+  pub title: &'static str,
+  pub size: (u32, u32),
 }
 
-/// An abstraction over a 'host' in a particular platform.
-pub trait Host<P: Platform> {
-  /// Ticks the host by a single frame, updating any platform systems and
-  /// advancing the game simulation via the given callback.
-  fn tick<C>(&mut self, callback: C)
-    where C: FnMut(&mut Self, f32) -> ();
+/// Executes the engine with the given configuration.
+pub fn run(config: Config) {
+  let surface = GlfwSurface::new(
+    WindowDim::Windowed(config.size.0, config.size.1),
+    config.title,
+    WindowOpt::default(),
+  );
+
+  match surface {
+    Ok(mut surface) => {
+      let back_buffer = surface.back_buffer().unwrap();
+
+      'app: loop {
+        for event in surface.poll_events() {
+          match event {
+            WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => break 'app,
+            _ => (),
+          }
+        }
+
+        surface.pipeline_builder().pipeline(
+          &back_buffer,
+          &PipelineState::default().set_clear_color([0.7, 0.1, 0.5, 1.]),
+          |_, _| (),
+        );
+
+        surface.swap_buffers();
+      }
+    }
+    Err(e) => {
+      eprintln!("Cannot create graphics surface:\n{}", e);
+      exit(-1);
+    }
+  }
 }
