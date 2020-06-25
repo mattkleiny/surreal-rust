337fb81ef4a8d9e5fdafc2d4d362ff8038c85aa9
Matt Kleinschafer <xeusalmighty@gmail.com>
2020-06-25 19:39:13 +1000

    Swap to SDL2, instead.

--- src/platform.rs
+++ src/platform.rs
@@ -1,14 +1,10 @@
 //! Platform abstractions and utilities.
 //!
-//! We export a minimum of our platform layer (Luminance) in order to make it simpler to use.
+//! We export a minimum of our platform layer.
 
 use std::time::Instant;
 
-use luminance::context::*;
-use luminance::pipeline::*;
-use luminance::pixel::*;
-use luminance::texture::*;
-use luminance_glfw::*;
+use sdl2::event::Event;
 
 use crate::graphics::Color;
 use crate::utilities::Clock;
@@ -28,44 +24,12 @@ pub struct Config<S> {
 ///
 /// This hides the inner implementation details of Luminance and makes
 /// it simpler to get sprites on the screen.
-pub struct Frame<'a> {
-  shading: ShadingGate<'a, GlfwSurface>
-}
+pub struct Frame {}
 
 /// Contains information on the game's timing state.
 #[derive(Copy, Clone, Debug)]
 pub struct GameTime {
   pub delta_time: f32,
-  pub frame: usize,
-}
-
-/// Represents an error in the platform layer.
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub enum Error {
-  GraphicsError,
-  TextureError,
-}
-
-impl From<GlfwSurfaceError> for Error {
-  fn from(error: GlfwSurfaceError) -> Self {
-    match error {
-      GlfwSurfaceError::InitError(_) => Error::GraphicsError,
-      GlfwSurfaceError::WindowCreationFailed => Error::GraphicsError,
-      GlfwSurfaceError::NoPrimaryMonitor => Error::GraphicsError,
-      GlfwSurfaceError::NoVideoMode => Error::GraphicsError,
-      GlfwSurfaceError::GraphicsStateError(_) => Error::GraphicsError,
-    }
-  }
-}
-
-impl From<TextureError> for Error {
-  fn from(error: TextureError) -> Self {
-    match error {
-      TextureError::TextureStorageCreationFailed(_) => Error::TextureError,
-      TextureError::NotEnoughPixels(_, _) => Error::TextureError,
-      TextureError::UnsupportedPixelFormat(_) => Error::TextureError,
-    }
-  }
 }
 
 /// Runs the game with the given configuration.
@@ -78,60 +42,73 @@ pub fn run<S, I, U, D>(
   where I: FnMut(&mut S, GameTime) -> (),
         U: FnMut(&mut S, GameTime) -> (),
         D: FnMut(&mut S, GameTime, Frame) -> () {
-  // build our window, this thing also handles our window events
-  let mut surface = GlfwSurface::new(
-    WindowDim::Windowed(config.size.0, config.size.1),
-    config.title,
-    WindowOpt::default(),
-  )?;
+  let context = sdl2::init()?;
+  let video = context.video()?;
 
-  let state = &mut config.state;
-  let back_buffer = surface.back_buffer()?;
+  let window = video.window(config.title, config.size.0, config.size.1)
+      .position_centered()
+      .resizable()
+      .build()?;
+
+  let mut canvas = window.into_canvas()
+      .present_vsync()
+      .accelerated()
+      .build()?;
+
+  let mut pump = context.event_pump()?;
   let mut clock = Clock::new(config.max_delta);
-  let mut frame = 0;
 
-  let texture = Texture::<Dim2, RGB8UI>::new(&mut surface, [256, 144], 0, Sampler {
-    wrap_r: Wrap::ClampToEdge,
-    wrap_s: Wrap::ClampToEdge,
-    wrap_t: Wrap::ClampToEdge,
-    min_filter: MinFilter::Nearest,
-    mag_filter: MagFilter::Nearest,
-    depth_comparison: None,
-  })?;
-
-  texture.clear(GenMipmaps::No, (255, 255, 255))?;
-
-  'game: loop {
-    // update the clock
-    let now = Instant::now().elapsed().as_secs();
-    let time = GameTime { delta_time: clock.tick(now, 60u64), frame };
-
-    frame += 1;
-
-    // update the underlying window
-    for event in surface.poll_events() {
+  'running: loop {
+    for event in pump.poll_iter() {
       match event {
-        WindowEvent::Close => break 'game,
-        WindowEvent::Key(_, _, _, _) => input(state, time),
-        _ => (),
+        Event::Quit { .. } => break 'running,
+        _ => {}
       }
     }
 
-    // update the game simulation
-    update(state, time);
+    let time = GameTime {
+      delta_time: clock.tick(Instant::now().elapsed().as_secs(), 60),
+    };
 
-    // draw this frame
-    surface.pipeline_builder().pipeline(
-      &back_buffer,
-      &PipelineState::default().set_clear_color(config.clear_color.into()),
-      |_, shading| {
-        draw(state, time, Frame { shading });
-      },
-    );
+    canvas.clear();
+    canvas.set_draw_color((255, 255, 255));
 
-    surface.swap_buffers();
+    input(&mut config.state, time);
+    update(&mut config.state, time);
+    draw(&mut config.state, time, Frame {});
+
+    canvas.present();
   }
 
   Ok(())
 }
 
+/// Represents an error in the platform layer.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Error {
+  GenericFailure
+}
+
+impl From<String> for Error {
+  fn from(_: String) -> Self {
+    Error::GenericFailure
+  }
+}
+
+impl From<sdl2::Error> for Error {
+  fn from(_: sdl2::Error) -> Self {
+    Error::GenericFailure
+  }
+}
+
+impl From<sdl2::video::WindowBuildError> for Error {
+  fn from(_: sdl2::video::WindowBuildError) -> Self {
+    Error::GenericFailure
+  }
+}
+
+impl From<sdl2::IntegerOrSdlError> for Error {
+  fn from(_: sdl2::IntegerOrSdlError) -> Self {
+    Error::GenericFailure
+  }
+}
