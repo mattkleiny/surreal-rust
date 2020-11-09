86c822de8790adb6e7957926e263047a016a3bfa
Matt Kleinschafer <xeusalmighty@gmail.com>
2020-11-09 21:21:13 +1100

    Even more work on the shady compiler.

--- src/graphics/shaders/spirv.rs
+++ src/graphics/shaders/spirv.rs
@@ -3,40 +3,57 @@ use super::shady::*;
 type CompileResult<T> = std::result::Result<T, CompileError>;
 
 /// A compiler for SPIR-V shader programs.
-pub struct SpirvCompiler;
+pub struct SpirvCompiler {
+  builder: SpirvBuilder
+}
 
 impl SpirvCompiler {
   pub fn compile(program: &ShadyProgram) -> CompileResult<SpirvProgram> {
-    let mut _compiler = Self::new();
-    let mut _result = SpirvProgram::new();
+    let mut compiler = SpirvCompiler {
+      builder: SpirvBuilder::new()
+    };
 
     for statement in &program.statements {
-      match statement {
-        Statement::Unknown => {}
-        Statement::Empty => {}
-        Statement::KindSpecification { .. } => {}
-        Statement::MethodDefinition { .. } => {}
-      }
+      compiler.visit_statement(statement);
+    }
+
+    Ok(compiler.builder.build())
+  }
+}
+
+impl super::shady::Visitor<()> for SpirvCompiler {
+  fn visit_statement(&mut self, statement: &Statement) -> () {
+    match statement {
+      Statement::Unknown => {}
+      Statement::Empty => {}
+      Statement::KindSpecification { .. } => {}
+      Statement::MethodDefinition { .. } => {}
     }
 
     unimplemented!()
   }
 
-  fn new() -> Self {
-    Self {}
+  fn visit_expression(&mut self, expression: &Expression) -> () {
+    unimplemented!()
   }
 }
 
+
 /// A SPIR-V compiled shader program.
-pub struct SpirvProgram {
-  binary: Vec<u8>,
+pub struct SpirvProgram(Vec<u8>);
+
+/// A procedural builder for SPIR-V programs.
+struct SpirvBuilder {
+  raw: Vec<u8>,
 }
 
-impl SpirvProgram {
-  fn new() -> Self {
-    Self {
-      binary: Vec::new()
-    }
+impl SpirvBuilder {
+  pub fn new() -> Self {
+    Self { raw: Vec::new() }
+  }
+
+  pub fn build(self) -> SpirvProgram {
+    SpirvProgram(self.raw)
   }
 }
 
