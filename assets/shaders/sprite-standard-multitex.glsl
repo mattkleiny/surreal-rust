60be55930c95687ede328cf2b44b51fcbc8cedbf
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-05-31 18:15:51 +1000

    Some more work on geometry batch.

---
+++ assets/shaders/sprite-standard-multitex.glsl
@@ -0,0 +1,37 @@
+// Implements a simple shader for sprites, with support for multiple textures per draw call.
+
+#version 330 core
+
+#shader_type vertex
+
+uniform mat4 u_projectionView = mat4(1.0);
+uniform vec4 u_color = vec4(1.0);
+
+layout(location = 0) in vec2 a_position;
+layout(location = 1) in vec2 a_uv;
+layout(location = 2) in vec4 a_color;
+layout(location = 3) in uint a_textureId;
+
+out vec2 v_uv;
+out vec4 v_color;
+out uint v_textureId;
+
+void main() {
+  v_uv        = a_uv;
+  v_color     = a_color * u_color;
+  v_textureId = a_textureId;
+
+  gl_Position = vec4(a_position, 0.0, 1.0) * u_projectionView;
+}
+
+#shader_type fragment
+
+uniform sampler2D[16] u_texture;
+
+in vec2 v_uv;
+in vec4 v_color;
+in flat uint v_textureId;
+
+void main() {
+  gl_FragColor = v_color * texture(u_texture[v_textureId], v_uv);
+}
--- src/graphics/geometry.rs
+++ src/graphics/geometry.rs
@@ -1,9 +1,7 @@
-use crate::maths::{Vector2, vec2, Rectangle, Circle};
+use crate::maths::{Rectangle, vec2, Vector2};
 
 use super::*;
 
-// Represents a point in the batch.
-type Point = Vector2<f32>;
 /// A fast and lightweight geometry batch renderer.
 ///
 /// This batch pre-allocates an array of vertices and re-uses it to tessellate shapes and polygons.
@@ -32,19 +30,21 @@ impl GeometryBatch {
   }
 
   /// Draws a line in the batch.
-  pub fn draw_line(&mut self, a: Point, b: Point, color: Color32, _thickness: f32) {
+  #[macros::profile_function]
+  pub fn draw_line(&mut self, a: Vector2<f32>, b: Vector2<f32>, color: Color32, _thickness: f32) {
     let base_offset = self.vertices.len() as Index;
 
     // TODO: get the winding order correct?
     self.vertices.push(Vertex2 { position: a, uv: vec2(0., 0.), color });
     self.vertices.push(Vertex2 { position: b, uv: vec2(1., 1.), color });
-    
+
     self.indices.push(base_offset + 0);
     self.indices.push(base_offset + 1);
   }
-  
+
   /// Draws a triangle in the batch.
-  pub fn draw_triangle(&mut self, a: Point, b: Point, c: Point, color: Color32) {
+  #[macros::profile_function]
+  pub fn draw_triangle(&mut self, a: Vector2<f32>, b: Vector2<f32>, c: Vector2<f32>, color: Color32) {
     let base_offset = self.vertices.len() as Index;
 
     self.vertices.push(Vertex2 { position: a, uv: vec2(0., 0.), color });
@@ -55,13 +55,14 @@ impl GeometryBatch {
     self.indices.push(base_offset + 1);
     self.indices.push(base_offset + 2);
   }
-  
+
   /// Draws a strip of triangles in the batch.
-  pub fn draw_triangle_strip(&mut self, points: &[Point], color: Color32) {
+  #[macros::profile_function]
+  pub fn draw_triangle_strip(&mut self, points: &[Vector2<f32>], color: Color32) {
     if points.len() < 3 { return; }
 
     let base_offset = self.vertices.len() as Index;
-    
+
     self.vertices.push(Vertex2 { position: points[0], uv: vec2(0., 0.), color });
 
     for i in 1..points.len() - 1 {
@@ -77,9 +78,10 @@ impl GeometryBatch {
   }
 
   /// Draws a rectangle in the batch.
+  #[macros::profile_function]
   pub fn draw_rectangle(&mut self, rectangle: Rectangle<f32>, color: Color32) {
     let base_offset = self.vertices.len() as Index;
-    
+
     self.vertices.push(Vertex2 { position: rectangle.bottom_left(), uv: vec2(0., 0.), color });
     self.vertices.push(Vertex2 { position: rectangle.top_left(), uv: vec2(0., 1.), color });
     self.vertices.push(Vertex2 { position: rectangle.top_right(), uv: vec2(1., 1.), color });
@@ -93,9 +95,10 @@ impl GeometryBatch {
     self.indices.push(base_offset + 2);
     self.indices.push(base_offset + 3);
   }
-  
+
   /// Draws a circle in the batch.
-  pub fn draw_circle(&mut self, center: Point, radius: f32, segments: u16, color: Color32) {
+  #[macros::profile_function]
+  pub fn draw_circle(&mut self, center: Vector2<f32>, radius: f32, segments: u16, color: Color32) {
     let mut points = Vec::with_capacity(segments as usize);
 
     for i in 0..segments {
@@ -109,13 +112,32 @@ impl GeometryBatch {
 
     self.draw_triangle_strip(&points, color);
   }
-  
+
   /// Draws a sprite in the batch.
-  pub fn draw_sprite(&mut self, _texture: &TextureRegion, _position: Point, _rotation: f32, _tint: Color32) {
-    todo!();
+  #[macros::profile_function]
+  pub fn draw_sprite(&mut self, texture: &TextureRegion, position: Vector2<f32>, scale: Vector2<f32>, color: Color32) {
+    let base_offset = self.vertices.len() as Index;
+
+    // calculate sprite bounds and uv coordinates
+    let bounds = Rectangle::from_size(position, scale);
+    let uv = texture.calculate_uv();
+
+    self.vertices.push(Vertex2 { position: bounds.bottom_left(), uv: uv.top_left(), color });
+    self.vertices.push(Vertex2 { position: bounds.top_left(), uv: uv.bottom_left(), color });
+    self.vertices.push(Vertex2 { position: bounds.top_right(), uv: uv.bottom_right(), color });
+    self.vertices.push(Vertex2 { position: bounds.bottom_right(), uv: uv.top_right(), color });
+
+    self.indices.push(base_offset + 0);
+    self.indices.push(base_offset + 1);
+    self.indices.push(base_offset + 2);
+
+    self.indices.push(base_offset + 0);
+    self.indices.push(base_offset + 2);
+    self.indices.push(base_offset + 3);
   }
 
   /// Flushes the batch content to the GPU.
+  #[macros::profile_function]
   pub fn flush(&mut self) {
     // ensure we're in a valid state to render something
     if self.vertices.len() == 0 { return; };
@@ -129,9 +151,9 @@ impl GeometryBatch {
     });
 
     self.mesh.draw_sub_mesh(
-      &material, 
-      PrimitiveTopology::Triangles, 
-      self.vertices.len(), 
+      &material,
+      PrimitiveTopology::Triangles,
+      self.vertices.len(),
       self.indices.len(),
     );
 
