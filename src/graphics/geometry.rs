99c1029835302810ca4a7eafa07725ebdbe87434
Matt Kleinschafer <matt.kleinschafer@xero.com>
2022-05-31 11:50:17 +1000

    Some more work on geometry batch.

--- src/graphics/geometry.rs
+++ src/graphics/geometry.rs
@@ -1,12 +1,9 @@
-use crate::maths::Vector2;
+use crate::maths::{Vector2, vec2};
 
 use super::*;
 
 // Represents a point in the batch.
 type Point = Vector2<f32>;
-
-/// A polygon that can be rendered in a geometry batch.
-pub type Polygon = Vec<Vertex2>;
 
 /// A shape that can be emitted in a geometry batch.
 pub trait Shape {
@@ -21,7 +18,6 @@ enum Command {
   DrawTriangleStrip(Vec<Point>),
   DrawQuad(Point, Point, Point, Point),
   DrawCircle(Point, f32, u16),
-  DrawPolygon(Polygon),
   DrawShape(Box<dyn Shape>),
 }
 
@@ -32,10 +28,22 @@ pub struct GeometryBatch {
   mesh: Mesh<Vertex2>,
   commands: Vec<Command>,
   vertices: Vec<Vertex2>,
+  indices: Vec<Index>,
   material: Option<Material>,
 }
 
 impl GeometryBatch {
+  /// Creates a new geometry batch.
+  pub fn new(server: &GraphicsServer) -> Self {
+    Self {
+      mesh: Mesh::new(server, BufferUsage::Dynamic),
+      commands: Vec::new(),
+      vertices: Vec::new(),
+      indices: Vec::new(),
+      material: None,
+    }
+  }
+
   /// Restarts the batch with the given material.
   pub fn begin(&mut self, material: &Material) {
     self.material = Some(material.clone());
@@ -68,11 +76,6 @@ impl GeometryBatch {
     self.commands.push(Command::DrawCircle(center, radius, segments));
   }
   
-  /// Draws a polygon in the batch.
-  pub fn draw_polygon(&mut self, polygon: Polygon) {
-    self.commands.push(Command::DrawPolygon(polygon));
-  }
-  
   /// Draws a shape in the batch.
   pub fn draw_shape(&mut self, shape: impl Shape + 'static) {
     self.commands.push(Command::DrawShape(Box::new(shape)));
@@ -80,25 +83,27 @@ impl GeometryBatch {
 
   /// Flushes the batch content to the GPU.
   pub fn flush(&mut self) {
-    // make sure we have something to write, first
-    if self.commands.len() == 0 { return; }
-    let Some(material) = &self.material else { return; };
-
     // tesselate all commands into vertices, first
     while let Some(command) = self.commands.pop() {
-      todo!()
+      self.tessellate(command);
     }
 
-    // upload vertices and draw the mesh
-    self.mesh.with_buffers(|vertices, _| {
+    // ensure we're in a valid state to render something
+    if self.vertices.len() == 0 { return; };
+    if self.indices.len() == 0 { return; };
+    let Some(material) = &self.material else { return; };
+
+    // upload and draw the mesh
+    self.mesh.with_buffers(|vertices, indices| {
       vertices.write_data(&self.vertices);
+      indices.write_data(&self.indices);
     });
 
     self.mesh.draw_sub_mesh(
       &material, 
       PrimitiveTopology::Triangles, 
       self.vertices.len(), 
-      0 // index count
+      self.indices.len(),
     );
 
     self.vertices.clear();
@@ -106,14 +111,49 @@ impl GeometryBatch {
 
   /// Tessellates the given command into vertices in the buffer
   fn tessellate(&mut self, command: Command) {
+    let offset = self.vertices.len() as Index;
+ 
     match command {
-      Command::DrawLine(a, b) => todo!(),
-      Command::DrawTriangle(a, b, c) => todo!(),
-      Command::DrawTriangleStrip(vertices) => todo!(),
-      Command::DrawQuad(a, b, c, d) => todo!(),
-      Command::DrawCircle(center, radius, segments) => todo!(),
-      Command::DrawPolygon(vertices) => todo!(),
-      Command::DrawShape(shape) => shape.emit(self),
+      Command::DrawLine(a, b) => {
+        // TODO: get the winding order correct?
+        self.vertices.push(Vertex2 { position: a, uv: vec2(0., 0.), color: Color32::WHITE });
+        self.vertices.push(Vertex2 { position: b, uv: vec2(1., 1.), color: Color32::WHITE });
+        
+        self.indices.push(offset + 0);
+        self.indices.push(offset + 1);
+      },
+      Command::DrawTriangle(a, b, c) => {
+        self.vertices.push(Vertex2 { position: a, uv: vec2(0., 0.), color: Color32::WHITE });
+        self.vertices.push(Vertex2 { position: b, uv: vec2(0.5, 1.), color: Color32::WHITE });
+        self.vertices.push(Vertex2 { position: c, uv: vec2(1., 0.), color: Color32::WHITE });
+
+        self.indices.push(offset + 0);
+        self.indices.push(offset + 1);
+        self.indices.push(offset + 2);
+      },
+      Command::DrawTriangleStrip(_vertices) => {
+        todo!()
+      },
+      Command::DrawQuad(a, b, c, d) => {
+        self.vertices.push(Vertex2 { position: a, uv: vec2(0., 0.), color: Color32::WHITE });
+        self.vertices.push(Vertex2 { position: b, uv: vec2(0., 1.), color: Color32::WHITE });
+        self.vertices.push(Vertex2 { position: c, uv: vec2(1., 1.), color: Color32::WHITE });
+        self.vertices.push(Vertex2 { position: d, uv: vec2(1., 0.), color: Color32::WHITE });
+
+        self.indices.push(offset + 0);
+        self.indices.push(offset + 1);
+        self.indices.push(offset + 2);
+
+        self.indices.push(offset + 0);
+        self.indices.push(offset + 2);
+        self.indices.push(offset + 3);
+      },
+      Command::DrawCircle(center, radius, segments) => {
+        todo!()
+      },
+      Command::DrawShape(shape) => {
+        shape.emit(self)
+      },
     }
   }
 }
