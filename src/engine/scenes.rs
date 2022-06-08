349b6b9f0c1fccc0714a7a634eeae0c9122136e5
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-06-08 23:50:19 +0000

    Some work on packed scenes

--- src/engine/scenes.rs
+++ src/engine/scenes.rs
@@ -3,11 +3,27 @@
 //! Scenes are serialized representations of entities and components
 //! that can be loaded and saved to disk.
 
-#[derive(Serialize, Deserialize)]
-pub struct Scene {}
+/// A scene that can be serialized/deserialized from disk.
+///
+/// This is a high level representation of the objects present in the scene,
+/// along with the parameters used to configure them.
+///
+/// A scene can be converted into a convential `Scene` via the `build()` method.
+#[derive(Default, Serialize, Deserialize)]
+pub struct PackedScene {}
 
-impl Scene {
-  pub fn new() -> Self {
-    Self {}
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  const SCENE_DOCUMENT: &str = r#"
+    objects:
+      - name: Player
+        archetype: Player
+  "#;
+
+  #[test]
+  fn packed_scene_should_be_loadable_from_yaml() {
+    todo!()
   }
 }
