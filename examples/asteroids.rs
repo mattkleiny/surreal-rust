77902e009b55498a2d2fd7e82c0d36dfb8e1d79f
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-06-26 23:56:11 +1000

    Some work on asteroids example

---
+++ examples/asteroids.rs
@@ -0,0 +1,117 @@
+//! An asteroids clone in Rust with manual rasterization into a pixel grid.
+
+use surreal::prelude::*;
+
+fn main() {
+  let configuration = Configuration {
+    title: "Asteroids",
+    log_level: LevelFilter::Trace,
+    transparent_window: true,
+    ..Default::default()
+  };
+
+  Engine::start(configuration, |engine, _| {
+    let mut canvas = PixelCanvas::<Color32>::new(&engine.graphics, 256, 144);
+    let polygon = Polygon::triangle(4.);
+
+    engine.run_variable_step(|engine, tick| {
+      engine.graphics.clear_color_buffer(Color::rgba(0.2, 0.2, 0.2, 0.8));
+
+      // TODO: why no longer drawing
+      canvas.pixels.draw(Color32::WHITE, &polygon);
+      canvas.draw();
+
+      if engine.input.keyboard.is_key_pressed(Key::Escape) {
+        tick.exit();
+      }
+    });
+  });
+}
+
+/// A polygon shape that can be rasterized to a `Grid`.
+struct Polygon {
+  vertices: Vec<Vector2<f32>>,
+}
+
+impl Polygon {
+  #[rustfmt::skip]
+  pub fn triangle(size: f32) -> Self {
+    Self {
+      vertices: vec![
+        vec2(-size, size),
+        vec2(0., -size),
+        vec2(size, size),
+      ],
+    }
+  }
+
+  pub fn bounds(&self) -> Rectangle<f32> {
+    let mut min_x = f32::MAX;
+    let mut min_y = f32::MAX;
+    let mut max_x = 0.;
+    let mut max_y = 0.;
+
+    for vertex in &self.vertices {
+      if vertex.x < min_x {
+        min_x = vertex.x
+      }
+      if vertex.y < min_y {
+        min_y = vertex.y
+      }
+      if vertex.x > max_x {
+        max_x = vertex.x
+      }
+      if vertex.y > max_x {
+        max_y = vertex.y
+      }
+    }
+
+    return Rectangle::from_corner_points(min_x, min_y, max_x, max_y);
+  }
+
+  pub fn contains(&self, point: Vector2<f32>) -> bool {
+    #[inline]
+    fn is_ccw(a: Vector2<f32>, b: Vector2<f32>, c: Vector2<f32>) -> bool {
+      (c.y - a.y) * (b.x - a.x) > (b.y - a.y) * (c.x - a.x)
+    }
+
+    #[inline]
+    fn intersects(a: Vector2<f32>, b: Vector2<f32>, c: Vector2<f32>, d: Vector2<f32>) -> bool {
+      is_ccw(a, c, d) != is_ccw(b, c, d) && is_ccw(a, b, c) != is_ccw(a, b, d)
+    }
+
+    let direction = Vector2 { x: 1000., ..point };
+    let mut count = 0;
+
+    for i in 0..self.vertices.len() {
+      let start = self.vertices[i];
+      let end = if i == self.vertices.len() - 1 {
+        self.vertices[0]
+      } else {
+        self.vertices[i + 1]
+      };
+
+      if intersects(point, direction, start, end) {
+        count += 1;
+      }
+    }
+
+    count % 2 == 1
+  }
+}
+
+impl Shape for Polygon {
+  fn rasterize<T: Clone>(&self, value: T, target: &mut Grid<T>) {
+    let bounds = self.bounds();
+
+    for y in bounds.top() as i32..bounds.bottom() as i32 {
+      for x in bounds.left() as i32..bounds.right() as i32 {
+        let point = vec2(x as f32, y as f32);
+
+        if self.contains(point) {
+          target.set(x, y, value.clone());
+        }
+      }
+    }
+  }
+}
