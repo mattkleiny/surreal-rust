a0f46d09dbd7ad65685dfb746a042400704e5aab
Matt Kleinschafer <xeusalmighty@gmail.com>
2023-01-01 17:28:36 +1100

    Some more work on type database

--- core/src/utilities/object.rs
+++ core/src/utilities/object.rs
@@ -1,5 +1,8 @@
 use std::any::Any;
 use std::borrow::Cow;
+use std::collections::HashMap;
+
+use super::impl_singleton;
 
 /// Represents an 'Object'.
 ///
@@ -37,6 +40,11 @@ impl Type {
   pub fn of<T: TypeOf>() -> Self {
     T::type_of()
   }
+
+  /// Retrieves the [`TypeMetadata`] for the type.
+  pub fn metadata(&self) -> Option<&TypeMetadata> {
+    TypeDatabase::instance().get(self)
+  }
 }
 
 impl From<&'static str> for Type {
@@ -52,6 +60,36 @@ impl From<String> for Type {
     Self(Cow::Owned(value))
   }
 }
+
+/// Metadata about a [`Type`].
+#[derive(Debug)]
+pub struct TypeMetadata {}
+
+/// A static [`Type`] database.
+///
+/// The database carries information about [`Object`]s and allows reflection and
+/// introspection of all available objects in a project (that have been registered).
+#[derive(Default, Debug)]
+pub struct TypeDatabase {
+  types: HashMap<Type, TypeMetadata>,
+}
+
+impl TypeDatabase {
+  /// Registers the given [`Type`] in the database.
+  pub fn register<T: TypeOf>(&mut self) {
+    let key = Type::of::<T>();
+    let value = TypeMetadata {};
+
+    self.types.insert(key, value);
+  }
+
+  /// Tries to get the [`TypeMetadata`] for the given [`Type`].
+  pub fn get(&self, key: &Type) -> Option<&TypeMetadata> {
+    self.types.get(key)
+  }
+}
+
+impl_singleton!(TypeDatabase);
 
 #[cfg(test)]
 mod tests {
@@ -71,5 +109,14 @@ mod tests {
     assert_eq!(object.get_type(), Type::from("TestObject"));
     assert_eq!(Type::of::<TestObject>(), Type::from("TestObject"));
     assert_eq!(TestObject::type_of(), Type::from("TestObject"));
+  }
+
+  #[test]
+  fn type_database_should_register_types() {
+    let database = TypeDatabase::instance();
+
+    database.register::<TestObject>();
+
+    println!("{:#?}", database);
   }
 }
