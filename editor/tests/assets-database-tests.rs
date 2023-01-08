1720fb9d86386b6251bd978c9576d1832e9a24a6
Matt Kleinschafer <xeusalmighty@gmail.com>
2023-01-09 08:46:39 +1100

    Extract common module in editor tests

--- editor/tests/assets-database-tests.rs
+++ editor/tests/assets-database-tests.rs
@@ -1,17 +1,16 @@
 #![feature(anonymous_lifetime_in_impl_trait)]
 
-use std::collections::HashMap;
-
 use serde::{Deserialize, Serialize};
 
 use surreal::io::{Deserializable, Serializable, VirtualPath};
 use surreal::macros::Object;
-use surreal::maths::FromRandom;
-use surreal_editor::{AssetDatabase, AssetHash, AssetId, AssetMetadata, AssetTypeMetadata, Resource};
+use surreal_editor::Resource;
+
+mod common;
 
 #[test]
 fn resources_with_differences_should_hash_differently() {
-  let mut database = AssetDatabaseBuilder::default()
+  let mut database = common::AssetDatabaseBuilder::default()
     .with_resource("memory://test1.json", TestResource { value: 1 })
     .with_resource("memory://test2.json", TestResource { value: 2 })
     .build();
@@ -22,48 +21,6 @@ fn resources_with_differences_should_hash_differently() {
 
   assert_eq!(hash1, hash2);
   assert_ne!(hash1, hash3);
-}
-
-/// A helper for building up an [`AssetDatabase`] for use in test cases.
-///
-/// When writing files, use the `memory://` scheme to minimize churn on the file system.
-#[must_use]
-struct AssetDatabaseBuilder {
-  assets: HashMap<String, AssetMetadata>,
-}
-
-impl Default for AssetDatabaseBuilder {
-  fn default() -> Self {
-    Self { assets: HashMap::new() }
-  }
-}
-
-impl AssetDatabaseBuilder {
-  /// Adds a resource to the [`AssetDatabase`] at the given [`VirtualPath`].
-  pub fn with_resource(mut self, path: impl Into<VirtualPath>, resource: impl Resource) -> Self {
-    let path = path.into();
-
-    self.assets.insert(
-      path.to_string(),
-      AssetMetadata {
-        id: AssetId::random(),
-        hash: AssetHash::from_resource(&resource).expect("Failed to hash asset"),
-        assets: vec![AssetTypeMetadata {
-          offset: 0,
-          kind: resource.get_type(),
-        }],
-      },
-    );
-
-    resource.save(path).expect("Failed to save resource");
-
-    self
-  }
-
-  /// Builds the resultant [`AssetDatabase`].
-  pub fn build(self) -> AssetDatabase {
-    AssetDatabase::from_metadata(self.assets.into_iter())
-  }
 }
 
 /// A simple test [`Resource`] for use in the [`AssetDatabase`].
