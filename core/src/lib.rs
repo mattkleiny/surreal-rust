71348212cd6f73cafc32319d1bf8d9b568c3e94f
Matt Kleinschafer <xeusalmighty@gmail.com>
2023-03-24 10:45:52 +1100

    Prepping to add benchmarks

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,14 +3,16 @@
 //! This crate contains common utilities, collections, diagnostics and other
 //! general purpose code for use in other systems.
 
+#![allow(incomplete_features)]
 #![feature(anonymous_lifetime_in_impl_trait)]
 #![feature(async_fn_in_trait)]
+#![feature(test)]
 #![feature(trivial_bounds)]
 #![feature(type_alias_impl_trait)]
-#![allow(incomplete_features)]
 
 #[macro_use]
 extern crate serde;
+extern crate test;
 
 pub use anyhow::{anyhow, bail, Error, Result};
 pub use macros;
