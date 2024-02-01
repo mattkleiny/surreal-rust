69022e4a7f7574c82979057c36ba4536cf3407b3
Matt Kleinschafer <matt.kleinschafer@karbonhq.com>
2024-02-02 08:54:03 +1100

    Stub some GBA backend bits

--- backends/gba/src/lib.rs
+++ backends/gba/src/lib.rs
@@ -1,10 +1,52 @@
 //! A backend for Surreal specifically for the GameBoy Advance.
 
-#![no_std] // GBA runs on bare metal, no stdlib
+#![no_std]
 
-pub struct GameBoy {}
+extern crate alloc;
 
-pub trait Display {
+/// The allocator used by the GameBoy runtime.
+///
+/// This is a simple bump allocator that allocates from a fixed-size heap.
+#[global_allocator]
+static HEAP_ALLOCATOR: embedded_alloc::Heap = embedded_alloc::Heap::empty();
+
+/// Entry point for the GameBoy runtime.
+///
+/// This function is called by the GameBoy runtime when the game is started.
+/// It is responsible for initializing the allocator and then starting the game.
+///
+/// The game should never return from this function.
+#[allow(dead_code)]
+fn main() -> ! {
+  initialize_heap_allocator();
+
+  loop {
+    todo!("run the main loop")
+  }
+}
+
+/// Initializes the heap allocator used by the runtime.
+///
+/// The hardware of the GBA is very limited, so we can't use a dynamic
+/// allocator. Instead, we use a simple bump allocator that allocates from a
+/// fixed-size heap.
+fn initialize_heap_allocator() {
+  use core::mem::MaybeUninit;
+
+  // allocate the main heap
+  const HEAP_SIZE: usize = 4069;
+  static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+
+  unsafe {
+    HEAP_ALLOCATOR.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE);
+  }
+}
+
+/// Represents the audio device of the GameBoy Advance.
+pub trait AudioDevice {}
+
+/// Represents the display of the GameBoy Advance.
+pub trait DisplayDevice {
   fn clear(&mut self);
   fn draw_pixel(&mut self, x: u32, y: u32, color: u32);
   fn draw_line(&mut self, x0: u32, y0: u32, x1: u32, y1: u32, color: u32);
@@ -19,13 +61,9 @@ pub trait Display {
   fn draw_sprite_centered(&mut self, x: u32, y: u32, sprite: &dyn Sprite);
 }
 
-pub trait Sprite {
-  fn width(&self) -> u32;
-  fn height(&self) -> u32;
-  fn pixel(&self, x: u32, y: u32) -> u32;
-}
+/// Represents the controller of the GameBoy Advance.
 
-pub trait Controller {
+pub trait InputDevice {
   fn is_button_down(&self, button: Button) -> bool;
   fn is_button_up(&self, button: Button) -> bool;
   fn is_button_pressed(&self, button: Button) -> bool;
@@ -34,6 +72,14 @@ pub trait Controller {
   fn is_button_not_held(&self, button: Button) -> bool;
 }
 
+/// Represents a sprite that can be drawn to the display.
+pub trait Sprite {
+  fn width(&self) -> u32;
+  fn height(&self) -> u32;
+  fn pixel(&self, x: u32, y: u32) -> u32;
+}
+
+/// A button on the GameBoy Advance.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Button {
   A,
