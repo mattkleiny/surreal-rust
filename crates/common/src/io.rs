7040bad9f767a2f52a408c7e859b52b8c31226b9
Matt Kleinschafer <xeusalmighty@gmail.com>
2024-07-22 17:05:06 +1000

    More work on PyxelEdit parser

--- crates/common/src/io.rs
+++ crates/common/src/io.rs
@@ -1,15 +1,15 @@
 //! Input/output utilities and virtual file system.
 
+pub use archives::*;
 pub use buffers::*;
 pub use compression::*;
 pub use formats::*;
 pub use streams::*;
 pub use virtualfs::*;
-pub use zips::*;
 
+mod archives;
 mod buffers;
 mod compression;
 mod formats;
 mod streams;
 mod virtualfs;
-mod zips;
