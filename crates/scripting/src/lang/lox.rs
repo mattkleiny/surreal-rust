1debaa94ff08de7bd575db8e9301da481d302f5a
Matt Kleinschafer <xeusalmighty@gmail.com>
2024-07-23 15:33:47 +1000

    Sketching pratt parser rules

--- crates/scripting/src/lang/lox.rs
+++ crates/scripting/src/lang/lox.rs
@@ -15,6 +15,7 @@ fn tokenize(code: &str) -> Vec<Token> {
   todo!()
 }
 
+/// Tokens from the Lox language
 enum Token {
   LeftParen,
   RightParen,
@@ -25,3 +26,38 @@ enum Token {
   Literal(Literal),
   Invalid(String),
 }
+
+/// Precedence levels for the Pratt parser
+enum Precedence {
+  None,
+  Assignment,
+  Or,
+  And,
+  Equality,
+  Comparison,
+  Term,
+  Factor,
+  Unary,
+  Call,
+  Primary,
+}
+
+/// Pratt parser rules for the Lox language
+struct ParseRule {
+  prefix: Option<fn()>,
+  infix: Option<fn()>,
+  precedence: Precedence,
+}
+
+macro_rules! rules {
+  ($($name:ident => $prefix:expr, $infix:expr, $precedence:expr),*) => {
+    &[
+      $(ParseRule { prefix: $prefix,infix: $infix, precedence: $precedence }),*
+    ]
+  };
+}
+
+const RULE_TABLE: &[ParseRule] = rules! {
+  Rule1 => None, None, Precedence::None,
+  Rule2 => None, None, Precedence::Assignment
+};
